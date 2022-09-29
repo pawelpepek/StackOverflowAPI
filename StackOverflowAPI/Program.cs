6c34d1eb4aff27fc4efb9a153ff16b3393ff0ac3
Paweł Pepek <pepek@riskout.pl>
2022-09-29 21:10:37 +0200

    UserService and user actions

--- StackOverflowAPI/Program.cs
+++ StackOverflowAPI/Program.cs
@@ -3,12 +3,14 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using StackOverflowAPI.Dtos;
 using StackOverflowAPI.Entities;
+using StackOverflowAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddAutoMapper(typeof(StackOverflowDbContext).Assembly);
+builder.Services.AddScoped<IUserService, UserService>();
 
 builder.Services.AddDbContext<StackOverflowDbContext>
     (o => o.UseSqlServer(builder.Configuration.GetConnectionString("DbConnectionString")));
@@ -24,22 +26,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.MapPost("/users", async (StackOverflowDbContext db, IMapper mapper, [FromBody] UserDto dto) =>
-{
-    if (db.Users.Any(u => u.Email.ToLower().Trim() == dto.Email.ToLower()))
-    {
-        throw new Exception("Email exists in database!");
-    }
-    else
-    {
-        dto.Email = dto.Email.Trim();
-        var user = mapper.Map<User>(dto);
-
-        await db.Users.AddAsync(user);
-        await db.SaveChangesAsync();
-
-        return user.Id;
-    }
-});
+app.MapPost("/users", async (IUserService service, [FromBody] UserDto dto) => await service.AddNewUser(dto));
+app.MapGet("/users", (IUserService service) => service.GetUsers());
+app.MapGet("/users/{email}", async (IUserService service, string email) => await service.GetUser(email));
+app.MapPut("/users", (IUserService service, [FromBody] UserDto dto) => service.UpdateUser(dto));
 
 app.Run();
---
+++ StackOverflowAPI/Services/UserService.cs
@@ -0,0 +1,84 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using StackOverflowAPI.Dtos;
+using StackOverflowAPI.Entities;
+
+namespace StackOverflowAPI.Services;
+
+public interface IUserService
+{
+    Task<UserDto> GetUser(string email);
+    Task<int> AddNewUser(UserDto dto);
+    List<UserDto> GetUsers();
+    Task UpdateUser(UserDto dto);
+}
+
+public class UserService : IUserService
+{
+    private readonly StackOverflowDbContext _db;
+    private readonly IMapper _mapper;
+
+    public UserService(StackOverflowDbContext db, IMapper mapper)
+    {
+        this._db = db;
+        this._mapper = mapper;
+    }
+
+    public async Task<int> AddNewUser(UserDto dto)
+    {
+        if (UserExists(dto.Email))
+        {
+            throw new Exception("Email exists in database!");
+        }
+        else
+        {
+            dto.Email = dto.Email.Trim();
+            var user = _mapper.Map<User>(dto);
+
+            await _db.Users.AddAsync(user);
+            await _db.SaveChangesAsync();
+
+            return user.Id;
+        }
+    }
+
+    public List<UserDto> GetUsers()
+    {
+        return _db.Users.AsNoTracking()
+               .OrderBy(u => u.Name)
+               .Select(_mapper.Map<UserDto>)
+               .ToList();
+    }
+
+    public async Task<UserDto> GetUser(string email)
+    {
+        return _mapper.Map<UserDto>(await FindUser(email));
+    }
+
+    public async Task UpdateUser(UserDto dto)
+    {
+        var user = await FindUser(dto.Email);
+
+        user.Name = dto.Name;
+
+        await _db.SaveChangesAsync();
+    }
+
+    private async Task<User> FindUser(string email)
+    {
+        var user = await _db.Users.FirstOrDefaultAsync(u => u.Email.ToLower() == email.ToLower().Trim());
+        if (user == null)
+        {
+            throw new Exception("User doesn't exist!");
+        }
+        else
+        {
+            return user;
+        }
+    }
+ 
+    private bool UserExists(string email)
+        => _db.Users.Any(u => u.Email.ToLower() == email.ToLower().Trim());
+
+
+}
